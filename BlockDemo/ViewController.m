cc484eac37dc9a5251f6cf38d9429420ebc35915
ankitoptimus <ankitkr.gupta@optimusinfo.com>
2015-08-18 13:00:29 +0530

    " Completion Handlers... "

--- BlockDemo/ViewController.m
+++ BlockDemo/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+-(void) test;
+
+-(void)addNumber:(int)number1 withNumber:(int)number2 andCompletionHandler:(void (^)(int result))completionHandler;
+
+
+
 @end
 
 @implementation ViewController
@@ -17,6 +23,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self test];
+    
+    [self addNumber:5 withNumber:7 andCompletionHandler:^(int result) {
+        // We just log the result, no need to do anything else.
+        NSLog(@"The result of add number block is : %d", result);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +36,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void) test{
+    __block int value=10;
+    int (^calc)(void) = ^(void){
+        value += 10;
+        return value;
+    };
+    NSLog(@" Result of test block after increment = %d",calc());
+}
+
+-(void)addNumber:(int)number1 withNumber:(int)number2 andCompletionHandler:(void (^)(int result))completionHandler{
+    int result = number1 + number2;
+    completionHandler(result);
+}
 @end
